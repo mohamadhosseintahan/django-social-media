f66afd75a235af9e2e4d51ad9337aaef32b989af
Mohamad <mohamadhosseintahan@yahoo.com>
2021-01-24 10:42:55 +0330

    form customizing done

--- social_media/account/forms.py
+++ social_media/account/forms.py
@@ -1,17 +1,23 @@
 from django import forms
 
+messages = {
+    'required': 'you should fill this field',
+    'invalid': 'your filled data is invalid',
+    'max_length': 'please decrement your entry',
+}
+
 
 class UserLoginForm(forms.Form):
-    username = forms.CharField(max_length=30,
+    username = forms.CharField(error_messages=messages, max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    password = forms.CharField(max_length=40,
+    password = forms.CharField(error_messages=messages, max_length=40,
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(max_length=30,
+    username = forms.CharField(error_messages=messages, max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
-    email = forms.EmailField(max_length=50,
+    email = forms.EmailField(error_messages=messages, max_length=50,
                              widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
-    password = forms.CharField(max_length=40,
+    password = forms.CharField(error_messages=messages, max_length=40,
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
