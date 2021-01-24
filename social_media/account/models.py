81dcec5116ddbf8938791e19c63097baeb788293
Mohamad <mohamadhosseintahan@yahoo.com>
2021-01-24 11:38:34 +0330

    signals added

--- social_media/account/models.py
+++ social_media/account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(null=True, blank=True)
+    age = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+def save_profile(sender, **kwargs):
+    if kwargs['created']:
+        p1 = Profile(user=kwargs['instance'])
+        p1.save()
+
+
+post_save.connect(save_profile, User)
